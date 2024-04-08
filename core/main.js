fe04bcd37300355aeffbb4da3e57429c735cf226
Bruno Fanini <phoenixbf@gmail.com>
2024-04-08 12:29:09 +0200

    bump

---
+++ core/main.js
@@ -0,0 +1,21 @@
+const fs          = require('fs');
+const http        = require('http');
+const express     = require('express');
+const path        = require('path');
+
+const PORT = 8095;
+const DIR_PUBLIC = path.join(__dirname,"../public/");
+const DIR_JSONFORM = path.join(__dirname,"../node_modules/jsonform/");
+
+let app = express();
+
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/', express.static(DIR_PUBLIC));
+
+app.use('/jsonform', express.static(DIR_JSONFORM));
+
+
+http.createServer(app).listen(PORT, ()=>{
+	console.log("Service up and running");
+});
