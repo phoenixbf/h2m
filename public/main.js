d23653b63fd09c889cbadca279d3568ecfb46320
Bruno Fanini <phoenixbf@gmail.com>
2024-10-06 22:26:39 +0200

    form update

--- public/main.js
+++ public/main.js
@@ -1,12 +1,136 @@
 let jschema = undefined;
+let dlink = undefined;
 
 let submit = (errors, values)=>{
     if (errors){
         console.log(errors);
         return;
     }
+    let ri = "";
+    let title = "";
 
-    $('#res').html("<pre>"+JSON.stringify(values)+"</pre>");
+    if (!values.provider || !values.provider.RI) return;
+    if (!values.general || !values.general.title) return;
+
+    ri = values.provider.RI.toLowerCase();
+    ri = ri.replace(/[^a-zA-Z ]/g, "");
+
+    title = values.general.title.toLowerCase();;
+    title = title.replace(/[^a-zA-Z ]/g, "");
+    title = title.replace(/\s+/g, '');
+
+    let srvid = ri + "-" + title;
+    console.log(srvid);
+
+    let strdata = JSON.stringify(values);
+
+    $('#res').html("<pre>"+strdata+"</pre>");
+
+    downloadAsJSON(srvid+".h2iosc.service.json", strdata);
+};
+
+let downloadAsJSON = (filename, data)=>{
+    dlink.href = URL.createObjectURL( new Blob( [ data ], { type: 'text/plain' } ) );
+    dlink.download = filename;
+    dlink.click();
+};
+
+/*
+    FORM
+==============================*/
+let formGeneral = {
+    type: "fieldset",
+    title: "General",
+    expandable: false,
+    items: [
+        {
+            key: "general.title",
+            type: "text"
+        },
+        {
+            key: "general.summary",
+            type: "textarea"
+        },
+
+        {
+            key: "general.category",
+            type: "checkboxes"
+        },
+
+        {
+            key: "general.website",
+            type: "text"
+        },
+    ]
+};
+
+let formProvider = {
+    type: "fieldset",
+    title: "Provider",
+    expandable: true,
+    items: [
+        {
+            key: "provider.RI",
+            type: "radiobuttons"
+        },
+        {
+            key: "provider.contact",
+            type: "array",
+            items:{
+                key: "provider.contact[]",
+                title: "Mail contact {{idx}}"
+            }
+        },
+    ]
+};
+
+let formFlows = {
+    type: "fieldset",
+    title: "Flows",
+    expandable: true,
+    items: [
+        {
+            key: "flows.list",
+            type: "array",
+            items:{
+                key: "flows.list[]",
+                title: "Flow #{{idx}}"
+            }
+        },
+    ]
+};
+
+let formAccess = {
+    type: "fieldset",
+    title: "Access",
+    expandable: true,
+    items: [
+        {
+            key: "access.instances",
+            type: "array",
+            //htmlClass: "...",
+            items:{
+                key: "access.instances[]",
+                title: "Access point {{idx}}"
+            }
+        },
+        {
+            key: "access.oas",
+            type: "text"
+        },
+        {
+            key: "access.horizontal",
+            type: "checkbox"
+        },
+        {
+            key: "access.gui",
+            type: "checkbox"
+        },
+        {
+            key: "access.presentation",
+            type: "checkbox"
+        },
+    ]
 };
 
 let generateForm = ()=>{
@@ -15,89 +139,39 @@ let generateForm = ()=>{
 
         form: [
             //"*",
-
-            // GENERAL
+/*
             {
-                type: "fieldset",
-                title: "General",
-                expandable: false,
-                items: [
+                type:"fieldset",
+                title:"Example of Tabs",
+                items:[
                     {
-                        key: "general.title",
-                        type: "text"
-                    },
-                    {
-                        key: "general.summary",
-                        type: "textarea"
-                    },
-
-                    {
-                        key: "general.category",
-                        type: "checkboxes"
-                    },
-                ]
-            },
+                        type:"tabs",
+                        id:"navtabs",
+                        items: [
+*/
+            // GENERAL
+            formGeneral,
 
             // PROVIDER
-            {
-                type: "fieldset",
-                title: "Provider",
-                expandable: false,
-                items: [
-                    {
-                        key: "provider.RI",
-                        type: "radiobuttons"
-                    },
-                    {
-                        key: "provider.contact",
-                        type: "array"
-                    },
-                ]
-            },
+            formProvider,
 
             // FLOWS
-            {
-                type: "fieldset",
-                title: "Flows",
-                expandable: false,
-                items: [
-                    {
-                        key: "flows.list",
-                        type: "array"
-                    },
-                ]
-            },
+            formFlows,
 
             // ACCESS
-            {
-                type: "fieldset",
-                title: "Access",
-                expandable: false,
-                items: [
-                    {
-                        key: "access.instances",
-                        type: "array"
-                    },
-                    {
-                        key: "access.oas",
-                        type: "text"
-                    },
-                    {
-                        key: "access.horizontal",
-                        type: "checkbox"
-                    },
-                    {
-                        key: "access.gui",
-                        type: "checkbox"
-                    },
-                    {
-                        key: "access.presentation",
-                        type: "checkbox"
-                    },
-                ]
-            }, 
+            formAccess,
 
+/*
+                        ]
+                    }
+                ]
+            },
+*/
             //===============================
+            {
+                type: "help",
+                helpvalue: "<strong>Click on <em>Generate Manifest</em></strong> when you're done"
+            },
             {
                 type: "submit",
                 title: "Generate Manifest"
@@ -110,6 +184,10 @@ let generateForm = ()=>{
 
 
 window.addEventListener("load",()=>{
+
+    dlink = document.createElement('a');
+    dlink.style.display = 'none';
+    document.body.appendChild( dlink ); // Firefox workaround, see #6594
 
     $.get("api/schema", (data)=>{
         jschema = data;
